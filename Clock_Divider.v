346dc2451337b2f5baa468046738df3411db5dab
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2022-09-07 15:52:47 +0200

    Update Clock_Divider.v

--- Clock_Divider.v
+++ Clock_Divider.v
@@ -1,11 +1,15 @@
-module moduleName #(
-parameter
+module ClkDiv #(
+parameter div_ratio_width = 5
 ) (
-input wire              i_ref_clk;
-input wire              i_rst_n;
-input wire              i_clk_en;
-input wire              i_div_ratio;
-output reg              o_div_clk;            
+input wire                               i_ref_clk,
+input wire                               i_rst_n,
+input wire                               i_clk_en,
+input wire  [div_ratio_width-1:0]        i_div_ratio,
+output reg                               o_div_clk            
 );
-    
+
+
+
+
+
 endmodule
