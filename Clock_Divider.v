932dac93304a89afe33c31fe3755dc464d310d92
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2022-09-07 12:35:26 +0200

    Create Clock_Divider.v

---
+++ Clock_Divider.v
@@ -0,0 +1,11 @@
+module moduleName #(
+parameter
+) (
+input wire              i_ref_clk;
+input wire              i_rst_n;
+input wire              i_clk_en;
+input wire              i_div_ratio;
+output reg              o_div_clk;            
+);
+    
+endmodule
